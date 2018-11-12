bb5690aec50ea04b1099b64cf5df08a85c93e60b
Vladimir Borisov <vladimir@stremio.com>
2018-11-12 14:04:36 +0200

    Better Docker build script

--- dist-utils/build-package.sh
+++ dist-utils/build-package.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-DISTRO=$1
+DISTRO=$(basename -- "$1")
+DEST_DIR=$PWD
+DISTROS_DIR="$(realpath "$(dirname -- "$0")/../distros")"
 
 usage() {
-    echo Usage: $0 '<distro>'
-    echo Available distros are:
-    ls -Qq1 ../distros | sed 's/^/ * /g'
+    printf "Usage: %s <distro>\n\nAvailable distros are:\n" $0
+    ls -Qq1 "$DISTROS_DIR" | sed 's/^/ * /g'
     exit 1
 }
 
-test -z "$DISTRO" && usage
+if [[ -z "$DISTRO" || "${DISTRO:0:1}" = "." ]]
+then usage
+fi
 
-DEST_DIR=$(pwd)
-cd "../distros/$DISTRO" || usage
+cd "$DISTROS_DIR/$DISTRO" &> /dev/null || usage
+
 source mkconfig.sh
 docker run --rm -v "$DEST_DIR:/app" -t "$(docker build -q .)" sh -c "$COPY_CMD"
