7867f65a9702de9ba77a0f43de1eb73ba70ccb97
Vladimir Borisov <borisov1024@gmail.com>
2018-12-05 17:54:57 +0200

    mpv029

--- mpv.h
+++ mpv.h
@@ -4,7 +4,7 @@
 #include <QtQuick/QQuickFramebufferObject>
 
 #include <mpv/client.h>
-#include <mpv/opengl_cb.h>
+#include <mpv/render_gl.h>
 #include <mpv/qthelper.hpp>
 
 class MpvRenderer;
@@ -13,28 +13,33 @@ class MpvObject : public QQuickFramebufferObject
 {
     Q_OBJECT
 
-    mpv::qt::Handle mpv;
-    mpv_opengl_cb_context *mpv_gl;
+    mpv_handle *mpv;
+    mpv_render_context *mpv_gl;
 
     friend class MpvRenderer;
 
 public:
+    static void on_update(void *ctx);
+
     MpvObject(QQuickItem * parent = 0);
     virtual ~MpvObject();
     virtual Renderer *createRenderer() const;
+
 public slots:
     void command(const QVariant& params);
     void setProperty(const QString& name, const QVariant& value);
     QVariant getProperty(const QString& name);
     void observeProperty(const QString& name);
+
 signals:
     void onUpdate();
     void mpvEvent(const QString& ev, const QVariant& value);
+
 private slots:
     void doUpdate();
     void on_mpv_events();
+
 private:
-    static void on_update(void *ctx);
     static void wakeup(void *ctx);
     void handle_mpv_event(mpv_event *event);
 };
