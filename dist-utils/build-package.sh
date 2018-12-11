938d486793cfe98062c17a4ed07bdf2945733d01
Vladimir Borisov <borisov1024@gmail.com>
2018-12-11 16:12:12 +0200

    build-package.sh accepts tag or branch to build against

--- dist-utils/build-package.sh
+++ dist-utils/build-package.sh
@@ -6,15 +6,24 @@ then DEST_DIR=$(realpath -- "$2")
 else DEST_DIR=$PWD
 fi
 
+if [ -n "$3" ]; then
+	BRANCH="$3"
+else
+	BRANCH=master
+fi
+
 DISTROS_DIR="$(realpath -- "$(dirname -- "$0")/../distros")"
 
 usage() {
-    printf "Usage: %s distro [output directory]\n" "$0"
+    printf "Usage: %s distro [output directory] [version]\n" "$0"
     printf "\ndistro\n\t\tThe name of the distribution to build a package for.\n"
     printf "\noutput directory\n\t\tThe directory where the package should be placed.\n"
     printf "\t\tIf omitted, the CWD is asumed as output directory.\n"
     printf "\nAvailable distros are:\n"
     ls -Qq1 "$DISTROS_DIR" | sed 's/^/ * /g'
+    printf "\nThe version is git branch/tag\n"
+    printf "\t You can see the available branches with git branch -r\n"
+    printf "\t You can see the available tags with git tag -l\n"
     exit 1
 }
 
@@ -25,4 +34,4 @@ fi
 cd "$DISTROS_DIR/$DISTRO" &> /dev/null || usage
 
 source mkconfig.sh
-docker run --privileged --rm -v "$DEST_DIR:/app" -t "$(docker build -q .)" sh -c "(su builduser -c './package.sh') && $COPY_CMD"
+docker run --privileged --rm -v "$DEST_DIR:/app" -t "$(docker build -q .)" sh -c "(su builduser -c \"./package.sh $BRANCH\") && $COPY_CMD"
--- distros/Fedora/package.sh
+++ distros/Fedora/package.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if [ -n "$1" ]; then
+	export BRANCH="$1"
+fi
 
 rpmdev-setuptree
 spectool --get-files -R stremio.spec
