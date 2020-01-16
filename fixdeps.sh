add565bcbaeff57c6698597cb0eb3f104dd9e78c
Vladimir borisov <vladimir@stremio.com>
2020-01-16 12:11:03 +0200

    fixed fixdeps

--- fixdeps.sh
+++ fixdeps.sh
@@ -12,15 +12,13 @@ fix_lib () {
 	otool -L $LIB | tail -n +2 | awk '$1 ~ /^\/usr\/local.*\.dylib$/ {  print $1 }' | while IFS= read -r dep_path; do
 		dep_name=$(basename "$dep_path")
 		fix_path="$DEST_DIR/$dep_name"
-		test -f $fix_path && continue
+		echo -e "\t$dep_name"
 		install_name_tool -change "$dep_path" "$FIX_DIR/$dep_name" "$LIB"
+		test -f $fix_path && continue
 		cp "$dep_path" "$fix_path"
 		chmod +w $fix_path
 		fix_lib $fix_path
 	done
 }
 
-#ls $DEST_DIR/*.dylib | while IFS= read -r dylib; do fix_lib "$dylib"; done
-
-echo $DEST_DIR
-echo $FIX_DIR
+ls $DEST_DIR/*.dylib | while IFS= read -r dylib; do fix_lib "$dylib"; done
