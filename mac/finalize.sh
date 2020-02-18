f1f901c2e70b35d254202bbdb3ee55a6419a013d
Vladimir Borisov <borisov1024@gmail.com>
2020-02-18 18:12:26 +0200

    Remove -always-overwrite argument to macdeployqt

--- mac/finalize.sh
+++ mac/finalize.sh
@@ -9,7 +9,7 @@ cp $(which node) $DEST_DIR/
 chmod +w $DEST_DIR/ffmpeg
 chmod +w $DEST_DIR/node
 
-macdeployqt ./stremio.app -always-overwrite -executable=./stremio.app/Contents/MacOS/ffmpeg -executable=./stremio.app/Contents/MacOS/node
+macdeployqt ./stremio.app -executable=./stremio.app/Contents/MacOS/ffmpeg -executable=./stremio.app/Contents/MacOS/node
 
 curl https://s3-eu-west-1.amazonaws.com/stremio-artifacts/four/$TAG/server.js > $DEST_DIR/server.js
 # ./mac/fix_osx_deps.sh "./stremio.app/Contents/Frameworks" "@executable_path/../Frameworks"
