1a64bf95a5ecc79cc2054d3a619ea0e9aeda23f0
Vladimir Borisov <borisov1024@gmail.com>
2020-02-18 18:51:59 +0200

    Copy mpv lib

--- mac/finalize.sh
+++ mac/finalize.sh
@@ -9,6 +9,8 @@ cp $(which node) $DEST_DIR/
 chmod +w $DEST_DIR/ffmpeg
 chmod +w $DEST_DIR/node
 
+cp ./deps/libmpv/mac/lib/*.dylib ./stremio.app/Contents/Frameworks
+
 macdeployqt ./stremio.app -executable=./stremio.app/Contents/MacOS/ffmpeg -executable=./stremio.app/Contents/MacOS/node
 
 curl https://s3-eu-west-1.amazonaws.com/stremio-artifacts/four/$TAG/server.js > $DEST_DIR/server.js
