5bd71855e6fe27db6bf1a5a042abad72d7473e86
Vladimir Borisov <borisov1024@gmail.com>
2020-02-19 11:04:11 +0200

    Changed the copy command

--- mac/finalize.sh
+++ mac/finalize.sh
@@ -9,8 +9,10 @@ cp $(which node) $DEST_DIR/
 chmod +w $DEST_DIR/ffmpeg
 chmod +w $DEST_DIR/node
 
-cp ./deps/libmpv/mac/lib/*.dylib ./stremio.app/Contents/Frameworks
+cp ./deps/libmpv/mac/lib/*.dylib ./stremio.app/Contents/Frameworks/
 
+# https://bugreports.qt.io/browse/QTBUG-57265
+# you don't want to be using always-overwrite in any version until Qt 5.11.3
 macdeployqt ./stremio.app -executable=./stremio.app/Contents/MacOS/ffmpeg -executable=./stremio.app/Contents/MacOS/node
 
 curl https://s3-eu-west-1.amazonaws.com/stremio-artifacts/four/$TAG/server.js > $DEST_DIR/server.js
