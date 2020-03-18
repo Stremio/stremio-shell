1f54ef5a211523adb179c0fa752ecf617ce32724
core1024 <borisov1024@gmail.com>
2020-03-18 19:42:39 +0200

    Compile libmpv

--- distros/Ubuntu/package.sh
+++ distros/Ubuntu/package.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+git clone https://github.com/mpv-player/mpv-build.git
+pushd mpv-build
+echo --enable-libmpv-shared > mpv_options
+./rebuild
+popd
+
 git clone https://github.com/Stremio/stremio-shell.git
 cd stremio-shell
 if [ -n "$1" ]; then
@@ -13,4 +19,4 @@ cp dist-utils/common/preremove ./preremove-pak
 
 make -f release.makefile clean
 make -f release.makefile
-sudo checkinstall --default --install=no --fstrans=yes --pkgname stremio --pkgversion "$(grep -oPm1 'VERSION=\K.+' stremio.pro)" --pkggroup video --pkglicense="MIT" --nodoc --pkgarch=$(dpkg --print-architecture) --requires="nodejs,libmpv1 \(\>=0.27.2\),qml-module-qt-labs-platform \(\>=5.9.5\),qml-module-qtquick-controls \(\>=5.9.5\),qml-module-qtquick-dialogs \(\>=5.9.5\),qml-module-qtwebchannel \(\>=5.9.5\),qml-module-qtwebengine \(\>=5.9.5\)" make -f release.makefile install
+sudo checkinstall --default --install=no --fstrans=yes --pkgname stremio --pkgversion "$(grep -oPm1 'VERSION=\K.+' stremio.pro)" --pkggroup video --pkglicense="MIT" --nodoc --pkgarch=$(dpkg --print-architecture) --requires="nodejs,qml-module-qt-labs-platform \(\>=5.9.5\),qml-module-qtquick-controls \(\>=5.9.5\),qml-module-qtquick-dialogs \(\>=5.9.5\),qml-module-qtwebchannel \(\>=5.9.5\),qml-module-qtwebengine \(\>=5.9.5\)" make -f release.makefile install
