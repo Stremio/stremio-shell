f2f40f73d5736742cf07025124766e471cbc7340
Vladimir Borisov <borisov1024@gmail.com>
2020-06-08 15:45:04 +0300

    More reliable versioning

--- distros/Fedora/mkconfig.sh
+++ distros/Fedora/mkconfig.sh
@@ -2,7 +2,15 @@
 
 TMPL_FILE=stremio.spec.tpl
 DEST_FILE=stremio.spec
+if [[ -z "$BRANCH" ]]; then
+    BRANCH=master
+fi
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+git stash
+git checkout "$BRANCH"
 VERSION="$(grep -oPm1 'VERSION=\K.+' ../../stremio.pro)"
+git checkout "$CURRENT_BRANCH"
+git stash pop
 
 sed '/^%description$/r../../dist-utils/common/description' "$TMPL_FILE" > "$DEST_FILE"
 sed -i '/^%post$/r../../dist-utils/common/postinstall' "$DEST_FILE"
