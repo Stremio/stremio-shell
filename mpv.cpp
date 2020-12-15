e784be080f0990e28597d5b68f9991787b13350f
Vladimir Borisov <borisov1024@gmail.com>
2020-12-15 15:14:26 +0200

    Send the ended reason

--- mpv.cpp
+++ mpv.cpp
@@ -264,11 +264,14 @@ void MpvObject::handle_mpv_event(mpv_event *event) {
             mpv_event_end_file *endFile = (mpv_event_end_file *)event->data;
             switch (endFile->reason) {
                 case MPV_END_FILE_REASON_ERROR:
+                    eventJson["reason"] = "error";
                     eventJson["error"] = mpv_error_string(endFile->error);
                     break;
-                case MPV_END_FILE_REASON_STOP:
+                case MPV_END_FILE_REASON_QUIT:
+                    eventJson["reason"] = "quit";
                     break;
                 default:
+                    eventJson["reason"] = "other";
                     break;
             }
             Q_EMIT mpvEvent("mpv-event-ended", eventJson);
