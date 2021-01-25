f0e291b0d040746d6e2c2571eafb57862f28d31b
Vladimir Borisov <borisov1024@gmail.com>
2021-01-25 15:14:28 +0200

    Fixed the reason why template files was broken

--- distros/ArchLinux/mkconfig.sh
+++ distros/ArchLinux/mkconfig.sh
@@ -3,7 +3,7 @@
 TMPL_FILE=stremio.install.tpl
 DEST_FILE=stremio.install
 cp "$TMPL_FILE" "$DEST_FILE"
-sed -i '/^post_install/r../../dist-utils/common/postinstall' "$TMPL_FILE"
+sed -i '/^post_install/r../../dist-utils/common/postinstall' "$DEST_FILE"
 sed -i '/^pre_remove/r../../dist-utils/common/preremove' "$DEST_FILE"
 sed -i '2,${/^#!/d}' "$DEST_FILE"
 
--- distros/Fedora/mkconfig.sh
+++ distros/Fedora/mkconfig.sh
@@ -15,7 +15,7 @@ if [[ -n "$STASHED" ]]; then
 fi
 
 cp "$TMPL_FILE" "$DEST_FILE"
-sed -i '/^%description$/r../../dist-utils/common/description' "$TMPL_FILE"
+sed -i '/^%description$/r../../dist-utils/common/description' "$DEST_FILE"
 sed -i '/^%post$/r../../dist-utils/common/postinstall' "$DEST_FILE"
 sed -i '/^test/r../../dist-utils/common/preremove' "$DEST_FILE"
 sed -i '2,${/^#!/d}' "$DEST_FILE"
