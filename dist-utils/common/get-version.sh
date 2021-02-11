547803ad8302223398b7f7e5623172e792a866a3
Vladimir Borisov <borisov1024@gmail.com>
2021-02-11 15:10:15 +0200

    Generate bundle

--- dist-utils/common/get-version.sh
+++ dist-utils/common/get-version.sh
@@ -2,4 +2,4 @@
 
 cd "$( dirname "${BASH_SOURCE[0]}" )/../.." >/dev/null 2>&1 
 
-sed -E '/STREMIO_SHELL_VERSION=/!d;s/^.*STREMIO_SHELL_VERSION="([^"]+)".*$/\1/g' CMakeLists.txt
+sed -E '/^project\(/!d;s/^.*VERSION "([^"]+)".*$/\1/g' CMakeLists.txt
