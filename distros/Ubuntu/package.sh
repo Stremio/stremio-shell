0a88bb0005ee16588068ccaf923926fdbcdf8896
rslgp <rslgp@cin.ufpe.br>
2024-06-12 17:30:04 -0300

    fix ubuntu compatibility xenial

--- distros/Ubuntu/package.sh
+++ distros/Ubuntu/package.sh
@@ -16,13 +16,25 @@ make -f release.makefile
 
 sudo apt install libmpv1 qml-module-qt-labs-platform qml-module-qtquick-controls qml-module-qtquick-dialogs qml-module-qtwebchannel qml-module-qtwebengine qml-module-qt-labs-folderlistmodel qml-module-qt-labs-settings libfdk-aac2
 
-# need to use wget and fix x64 arch
+# libssl1.1 TEMPORARY WORKAROUND
 #OpenSSL 1.1.1 End of Life MAR 28TH, 2023
 # NEED UPDATE TO libssl3 and modify dependecy on libcrypto.so.1.1
 wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1-1ubuntu2.1~18.04.23_amd64.deb
 sudo dpkg -i libssl1.1_1.1.1-1ubuntu2.1~18.04.23_amd64.deb
 rm libssl1.1_1.1.1-1ubuntu2.1~18.04.23_amd64.deb
 # WARNING STREMIO uses libcrypto.so.1.1 but pkg libssl1.1 (DarkmatterUAE commented 2 weeks ago) no longer being maintained by OpenSSL dev. https://www.openssl.org/blog/blog/2023/03/28/1.1.1-EOL/
+
+# Ubuntu Xenial compatibility
+wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.0.0_1.0.2g-1ubuntu4.20_amd64.deb
+sudo dpkg -i libssl1.0.0_1.0.2g-1ubuntu4.20_amd64.deb
+rm libssl1.0.0_1.0.2g-1ubuntu4.20_amd64.deb
+
+# latest ubuntu compatibility
+wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.22_amd64.deb
+sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2.22_amd64.deb
+rm libssl1.1_1.1.1f-1ubuntu2.22_amd64.deb
+# END libssl1 TEMPORARY WORKAROUND
+
 
 sudo checkinstall --default --install=no --fstrans=yes --pkgname stremio --pkgversion "$(./dist-utils/common/get-version.sh)" --pkggroup video --pkglicense="MIT" --nodoc --pkgarch=$(dpkg --print-architecture) --requires="nodejs,libmpv1 \(\>=0.30.0\),qml-module-qt-labs-platform \(\>=5.9.5\),qml-module-qtquick-controls \(\>=5.9.5\),qml-module-qtquick-dialogs \(\>=5.9.5\),qml-module-qtwebchannel \(\>=5.9.5\),qml-module-qtwebengine \(\>=5.9.5\),qml-module-qt-labs-folderlistmodel \(\>=5.9.5\),qml-module-qt-labs-settings \(\>=5.9.5\),librubberband2 \(\>=1.8.1\),libuchardet0  \(\>=0.0.6\),libfdk-aac2 \(\>=2.0.2\),libssl1.1 \(\>=1.1.1\)" make -f release.makefile install
 
