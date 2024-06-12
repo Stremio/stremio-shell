3917909531ef13641c07688127f1ac78a4c92d0e
rslgp <rslgp@cin.ufpe.br>
2024-06-12 16:36:45 -0300

    missing pkg

--- distros/Ubuntu/package.sh
+++ distros/Ubuntu/package.sh
@@ -14,7 +14,7 @@ cp dist-utils/common/preremove ./preremove-pak
 make -f release.makefile clean
 make -f release.makefile
 
-sudo apt install libmpv1 qml-module-qt-labs-platform qml-module-qtquick-controls qml-module-qtquick-dialogs qml-module-qtwebchannel qml-module-qtwebengine qml-module-qt-labs-folderlistmodel qml-module-qt-labs-settings libssl1.1
+sudo apt install libmpv1 qml-module-qt-labs-platform qml-module-qtquick-controls qml-module-qtquick-dialogs qml-module-qtwebchannel qml-module-qtwebengine qml-module-qt-labs-folderlistmodel qml-module-qt-labs-settings libfdk-aac2 libssl1.1
 
 sudo checkinstall --default --install=no --fstrans=yes --pkgname stremio --pkgversion "$(./dist-utils/common/get-version.sh)" --pkggroup video --pkglicense="MIT" --nodoc --pkgarch=$(dpkg --print-architecture) --requires="nodejs,libmpv1 \(\>=0.30.0\),qml-module-qt-labs-platform \(\>=5.9.5\),qml-module-qtquick-controls \(\>=5.9.5\),qml-module-qtquick-dialogs \(\>=5.9.5\),qml-module-qtwebchannel \(\>=5.9.5\),qml-module-qtwebengine \(\>=5.9.5\),qml-module-qt-labs-folderlistmodel \(\>=5.9.5\),qml-module-qt-labs-settings \(\>=5.9.5\),librubberband2 \(\>=1.8.1\),libuchardet0  \(\>=0.0.6\),libfdk-aac2 \(\>=2.0.2\),libssl1.1 \(\>=1.1.1\)" make -f release.makefile install
 
