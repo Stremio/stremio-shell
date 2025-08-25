ce9ae1d08b5cc70b2895e745eeead8b41c849d8b
Vladimir Borisov <borisov1024@gmail.com>
2025-08-25 11:45:14 +0300

    Sign the app

--- sign.ps1
+++ sign.ps1
@@ -15,6 +15,11 @@ $thread = Start-ThreadJob -InputObject ($pw) -ScriptBlock {
     }
 }
 
-Get-AuthenticodeSignature windows\*.dll | Where-Object -Property Status -Value NotSigned -EQ | % { signtool sign /fd SHA256 /t http://timestamp.digicert.com /n "Smart Code OOD" $_.Path }
+Get-AuthenticodeSignature dist-win\*.exe | Where-Object -Property Status -Value NotSigned -EQ | ForEach-Object { signtool sign /fd SHA256 /t http://timestamp.digicert.com /n "Smart Code OOD" $_.Path }
+Get-AuthenticodeSignature dist-win\*.dll | Where-Object -Property Status -Value NotSigned -EQ | ForEach-Object { signtool sign /fd SHA256 /t http://timestamp.digicert.com /n "Smart Code OOD" $_.Path }
 
+$env:package_version = (Select-String -Path .\CMakeLists.txt -Pattern '^project\(stremio VERSION "([^"]+)"\)').Matches.Groups[1].Value
+
+&"C:\Program Files (x86)\NSIS\makensis.exe" windows\installer\windows-installer.nsi
+&signtool sign /fd SHA256 /t http://timestamp.digicert.com /n "Smart Code OOD" "Stremio $env:package_version.exe"
 Stop-Job -Job $thread
